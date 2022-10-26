a0f23e40443e8ee3d020aeef7a2172347d03c03e
pedrolucasgp <pedrolucasgoncalvespedro@gmail.com>
2022-10-26 17:47:40 -0300

    Começo NAVBAR

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eu Que Fizz</title>
+    <link rel="icon" href="./img/logo.png">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="./img/logo.png" alt="" width="60" height="48" ></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Loja
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Cadernos</a></li>
+                    <li><a class="dropdown-item" href="#">Planners</a></li>
+                    <li><a class="dropdown-item" href="#">Bottons</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Contato</a>
+                </li>
+            </ul>
+            </div>
+            <button class="btn btn-login" type="login">Login</button>
+        </div>
+    </nav>
+    <h1>TESTE</h1>
+<script src="./js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
